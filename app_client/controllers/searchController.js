cbc1767ce3159672c34448a6d3c4de4b8642ef89
Kris Kane <kkane106@gmail.com>
2016-06-10 08:42:49 -0600

    clean up superfluous code in ctrl

--- app_client/controllers/searchController.js
+++ app_client/controllers/searchController.js
@@ -7,7 +7,6 @@ angular.module('studentTracker')
     $scope.selected = undefined;
     
     $scope.students = [];
-    // ["Ms. Dan Armstrong", "Randy Ziemann", "Antonina Toy", "Keegan Bernier", "Cole Ledner", "Mr. Kaelyn Runte", "Dario Hodkiewicz PhD", "Toy Kuphal", "Noemie Fadel", "Pietro Eichmann Sr.", "Rebeka Padberg", "Daisha Oberbrunner PhD", "Claudie Emard V", "Trevor Heidenreich", "Mr. Jamey Rath", "Sheila Weimann", "Daphne Bartoletti", "Jackeline Pollich", "Kari Fritsch", "Ronaldo Klocko"]
 
     $scope.onSelect = function($item, $model, $label){
       // $item : full obj // $model : full obj // $label : just name
@@ -15,11 +14,7 @@ angular.module('studentTracker')
     }
 
     $scope.getStudents = function(val) {
-      return studentData.getStudentSearchDict(val)
-      .then(function(res){
-        console.log(res);
-        return res;
-      });
+      return studentData.getStudentSearchDict(val);
     };
 
     $scope.ngModelOptionsSelected = function(value) {
